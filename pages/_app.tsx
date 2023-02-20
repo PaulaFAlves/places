dd26d66b9c08512b6ede8cb899a6519db0c5dae0
paulafalves <paulafernandaalves@icloud.com>
2023-02-20 10:15:23 -0300

    feat: create context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { useState } from "react"
+import initialValues from "./api/data.json"
+import MyContext from "../src/context/myContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [placesData, setPlacesData] = useState(initialValues)
+
+  return (
+    <MyContext.Provider
+      value={{
+        placesData: placesData,
+        setPlacesData: setPlacesData,
+      }}
+    >
+      <Component {...pageProps} />
+    </MyContext.Provider>
+  )
 }
 export default MyApp
